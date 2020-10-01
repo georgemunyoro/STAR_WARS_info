c42d571dbc5af02e542fc2dd29d7ab15a71fd071
George Munyoro <george.guva@outlook.com>
2020-10-01 13:22:34 +0200

    Updated file search function to use json files instead of old csv files

--- utils.py
+++ utils.py
@@ -1,16 +1,17 @@
-import csv
+import json
 from typing import List, Dict
 
 
-def search_csv_file(files_to_search: List[str], **kwargs) -> List[Dict[str, str]]:
+def search_json_files(files_to_search: List[str], **kwargs) -> List[Dict[str, str]]:
     """
     Returns a list of rows that match the given parameters
     """
     matching_rows = []
     for filepath in files_to_search:
-        with open(filepath) as csv_file:
-            reader = csv.DictReader(csv_file)
+        with open(filepath) as json_file:
+            reader = json.load(json_file)
             for row in reader:
+                row = reader[row]
                 for key, value in kwargs.items():
                     if type(value) == int:
                         value = str(value)
@@ -24,7 +25,7 @@ def search_csv_file(files_to_search: List[str], **kwargs) -> List[Dict[str, str]
 def search(entity_type: str = 'any', **kwargs) -> List[Dict[str, str]]:
     entity_types = ['vehicles', 'species', 'starships', 'planets', 'characters']
     if entity_type == 'any':
-        return search_csv_file([f'./data/{t}.csv' for t in entity_types], **kwargs)
-    return search_csv_file(
-        [f'./data/{t}.csv' for t in entity_types if entity_type == t] if entity_type != 'all' else [], **kwargs
+        return search_json_files([f'./data/{t}.json' for t in entity_types], **kwargs)
+    return search_json_files(
+        [f'./data/{t}.json' for t in entity_types if entity_type == t] if entity_type != 'all' else [], **kwargs
     )
