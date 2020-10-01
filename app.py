adb77e598165a5e552a7f7c1055d099f68a468fc
George Munyoro <george.guva@outlook.com>
2020-10-01 09:50:16 +0200

    Created search route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from markupsafe import escape
+import utils
 
 app = Flask(__name__)
 
@@ -14,6 +15,22 @@ def about():
 @app.route('/user/<path:username>')
 def greeting(username):
     return " Hello, {}".format(escape(username))
+
+@app.route('/search/')
+def search():
+    print(dict(request.args))
+    if request.args.get('type') == 'characters':
+        return {'items': utils.search_characters(**dict(request.args))}
+    elif request.args.get('type') == 'vehicle':
+        return {'items': utils.search_vehicles(**dict(request.args))}
+    elif request.args.get('type') == 'species':
+        return {'items': utils.search_species(**dict(request.args))}
+    elif request.args.get('type') == 'starships':
+        return {'items': utils.search_starships(**dict(request.args))}
+    elif request.args.get('type') == 'planets':
+        return {'items': utils.search_planets(**dict(request.args))}
+    else:
+        return {'items': utils.search_all(**dict(request.args))}
 
 @app.errorhandler(404)
 def page_not_found(error):
